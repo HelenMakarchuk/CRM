3e1617c91faaf875b5d19b76c6e68b1352622bf6
Helena Makarchuk <helenamakarchuk@gmail.com>
2018-05-22 10:45:25 +0300

    1st release

--- WebAPI/Models/Constants.cs
+++ WebAPI/Models/Constants.cs
@@ -10,7 +10,7 @@ namespace WebAPI.Models
         //appsettings.json can't be used when deploying on Azure (or I didn't find workaround), so I decided to use Constants.cs
         public static String DBConnectionString {
             get {
-                return "Data Source=crmuniversityproject.database.windows.net;Initial Catalog=CRM_DB;Persist Security Info=False;User ID=helenamakarchuk;Password=*********;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+                return "Data Source=crmuniversityproject.database.windows.net;Initial Catalog=CRM_DB;Persist Security Info=False;User ID=helenamakarchuk;Password=gdhfb68t97BdfH;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
             }
         }
     }
